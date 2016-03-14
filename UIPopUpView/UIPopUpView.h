3cf898f369687e8d1b6445eaae18b1665b76a885
AlexsJones <jonesax@hush.com>
2016-03-14 15:26:46 +0000

    Initial Commit

---
+++ UIPopUpView/UIPopUpView.h
@@ -0,0 +1,19 @@
+//
+//  UIPopUpView.h
+//  UIPopUpView
+//
+//  Created by Alex Jones on 14/03/2016.
+//  Copyright © 2016 Alex Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIPopUpView.
+FOUNDATION_EXPORT double UIPopUpViewVersionNumber;
+
+//! Project version string for UIPopUpView.
+FOUNDATION_EXPORT const unsigned char UIPopUpViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIPopUpView/PublicHeader.h>
+
+
